fb58970aeeda8753f127adb79810b4da05cd8e5e
petch <petch.kruapanich@dga.or.th>
2026-04-27 14:26:26 +0700

    Initial Commit

---
+++ WavToMp3Converter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WavToMp3Converter
+//
+//  Created by Petch Kruapanich on 27/4/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WavToMp3Converter/WavToMp3ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WavToMp3ConverterApp.swift
+//  WavToMp3Converter
+//
+//  Created by Petch Kruapanich on 27/4/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct WavToMp3ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
